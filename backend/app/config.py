aa86200823104c001bd3a00921c7a2fa8dd81c65
Egehan Yıldız <egehanyldz7@gmail.com>
2024-08-22 15:23:39 +0300

    Vercel Deployment Needs 2.

--- backend/app/config.py
+++ backend/app/config.py
@@ -7,6 +7,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         'DATABASE_URL', 
-        'mysql://admin:egehan2004@advance-search-iztodb.cd2myey0qssm.eu-north-1.rds.amazonaws.com/iztodb'
+        'mysql+pymysql://admin:egehan2004@advance-search-iztodb.cd2myey0qssm.eu-north-1.rds.amazonaws.com/iztoDb'
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
