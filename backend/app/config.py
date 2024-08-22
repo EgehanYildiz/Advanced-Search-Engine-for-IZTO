22352000649373987488752cdeccb4979adfe439
Egehan Yıldız <egehanyldz7@gmail.com>
2024-08-22 15:37:37 +0300

    Render Deployment Needs 1.

--- backend/app/config.py
+++ backend/app/config.py
@@ -7,6 +7,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get(
         'DATABASE_URL', 
-        'mysql+pymysql://admin:egehan2004@advance-search-iztodb.cd2myey0qssm.eu-north-1.rds.amazonaws.com/iztodb'
+        'mysql://admin:egehan2004@advance-search-iztodb.cd2myey0qssm.eu-north-1.rds.amazonaws.com/iztodb'
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
